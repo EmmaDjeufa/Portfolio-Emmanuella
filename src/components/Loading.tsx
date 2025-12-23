58468bc858ac607f76d569be382da1c61114e85b
Emmanuella N. D. <57154645+EmmaDjeufa@users.noreply.github.com>
2025-12-23 15:17:44 +0100

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Cloud</span> <span>Developer</span>
-            <span> A Creative Cloud</span> <span>Developer</span>
+            <span> A Creative Cloud</span> <span>Engineer</span>
+            <span> A Creative Cloud</span> <span>Engineer</span>
           </Marquee>
         </div>
         <div
